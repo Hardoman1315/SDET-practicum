007f28e23add37d73b7215f256bc96f85e5ed11e
Hardoman1315 <hardomangithub@gmail.com>
2025-03-30 16:01:37 +0400

    CI/CD fixes

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -7,7 +9,14 @@ from selenium.webdriver.chrome.options import Options
 def driver():
     options = Options()
     options.add_argument('--enable-javascript')
-    _driver = webdriver.Chrome(options = options)
+
+    if os.getenv('CI_ENV') == 'true':
+        options.add_argument("--headless=new")  # Включаем headless-режим только для CI/CD
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+    _driver = webdriver.Chrome(options=options)
     _driver.minimize_window()
     yield _driver
     _driver.quit()
